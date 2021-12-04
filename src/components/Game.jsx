8284069f4bf6e59598ee619f87cfbba8a6e445a6
Midas <goncalomidoes@tecnico.ulisboa.pt>
2021-12-04 23:52:10 +0000

    Shop + Profile  Deploy

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,42 +1,21 @@
 import React, { useState , useEffect} from "react";
 import {Container , Row , Col, Spinner} from "react-bootstrap"
-import Board from "./Board"
 import supabaseClient from "../utils/supabaseClient";
 import { Navigate, useLocation } from "react-router-dom"
 
 function Game() {
     let location = useLocation()
-    const [cells, setCells] = useState(undefined);
 
-    useEffect(() => {
-        var json = require('./cells.json'); 
-        setCells(json)
-        //selectTodos()
-    },[]);  
     if(supabaseClient.auth.user()===null){
         return(<Navigate to="/login" state={{ from: location }}/>)
     }
-
-    if(cells === undefined ){
-        return (
+        return(
             <Container>
                 <Row className="text-center mb-4">
-                    <Col>
-                        <Spinner className="mt-3" animation="border" role="status">
-                            <span className="visually-hidden">Loading...</span>
-                        </Spinner>
-                    </Col>
+                    <Col><h1 className="display-4 font-weight-normal" >:( 404 Page not found :(</h1></Col>
                 </Row>
-            </Container>      
+            </Container>
         );
-    } 
-    else {
-        return(
-            <Container fluid>
-                <Row className="text-center mb-4">  
-                    <Board board = {cells.cells}/>  
-                </Row>
-            </Container> );
-    }
+
 }
 export default Game;
